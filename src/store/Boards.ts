3fe9fd61a4efe3862b3b67551b556bd221d35f2a
parmeped <parmeggianipedro@gmail.com>
2020-10-02 18:27:07 -0300

    Fuck warnings

--- src/store/Boards.ts
+++ src/store/Boards.ts
@@ -1,4 +1,4 @@
-import { Action, Reducer } from 'redux';
+import { Reducer } from 'redux';
 import { AppThunkAction } from '.';
 import { REQUEST_BOARDS, RECEIVE_BOARDS } from '../constants/action-types'
 
